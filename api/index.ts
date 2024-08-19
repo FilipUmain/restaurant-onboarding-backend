62e31be63b2f62d1ff7c40f527463f2b4b61f3b8
Filip Karlström <filip.karlstrom@umain.com>
2024-08-19 09:56:22 +0200

    added /data for test

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,9 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => res.send("Express on Vercel"));
+app.get("/", (req: Request, res: Response) => res.send("Hej"));
+
+app.get("/data", (req: Request, res: Response) => res.send("Data: Hej hej"));
 
 app.listen(3000, () => console.log("Server running on port 3000."));
 
