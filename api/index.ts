c25d669bc0dc4c3443113840622e955234bf08ea
Filip Karlström <filip.karlstrom@umain.com>
2024-08-19 10:13:29 +0200

    get all restaurants and restaurant by id

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,66 @@
 import express, { Request, Response } from "express";
+import { Restaurant } from "../types/Restaurant";
 
 const app = express();
+const restaurants: Restaurant[] = [
+  {
+    id: "1",
+    name: "Gourmet Burger Bistro",
+    rating: 4.7,
+    filter_ids: ["1"],
+    image_url: "https://example.com/images/gourmet-burger-bistro.jpg",
+    delivery_time_minutes: 35,
+    price_range_id: "2",
+  },
+  {
+    id: "2",
+    name: "Pasta Palace",
+    rating: 4.5,
+    filter_ids: ["2"],
+    image_url: "https://example.com/images/pasta-palace.jpg",
+    delivery_time_minutes: 30,
+    price_range_id: "3",
+  },
+  {
+    id: "3",
+    name: "Sushi Central",
+    rating: 4.9,
+    filter_ids: ["3"],
+    image_url: "https://example.com/images/sushi-central.jpg",
+    delivery_time_minutes: 45,
+    price_range_id: "4",
+  },
+  {
+    id: "4",
+    name: "Veggie Delight",
+    rating: 4.3,
+    filter_ids: ["1"],
+    image_url: "https://example.com/images/veggie-delight.jpg",
+    delivery_time_minutes: 25,
+    price_range_id: "1",
+  },
+  {
+    id: "5",
+    name: "Steakhouse Supreme",
+    rating: 4.8,
+    filter_ids: ["5"],
+    image_url: "https://example.com/images/steakhouse-supreme.jpg",
+    delivery_time_minutes: 50,
+    price_range_id: "5",
+  },
+];
 
 // Get all restaurants
-app.get("/restaurants", (req: Request, res: Response) =>
-  res.send("Data: Hej hej")
-);
+app.get("/restaurants", (req: Request, res: Response) => res.send(restaurants));
 
 // Get restaurant by id
-app.get("/restaurants/:id", (req: Request, res: Response) =>
-  res.send("reastaurant")
-);
+app.get("/restaurants/:id", (req: Request, res: Response) => {
+  const id = JSON.parse(req.params.id);
+
+  const item = restaurants.find((item: Restaurant) => item.id === id);
+
+  res.send(item);
+});
 
 // Get filters
 app.get("/filter", (req: Request, res: Response) => res.send("filter"));
